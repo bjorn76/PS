421eac8039e01c8b0f198c0053935092e3c49b18
bjorn76 <bjorn.k.j@gmail.com>
2019-10-20 11:48:57 +0200

    B2

--- date.ps1
+++ date.ps1
@@ -1,30 +1,41 @@
 ﻿
+cls
+Write-Output "The basic:" -
+# Creaates a date object with current date and time
+$today = get-date
+Write-Output "$today"
 
-$thisyear = get-date -Format yyyy
 
+Write-Output "Fixed dates:"
+
+#Creates a fixed date time
+$adate = get-date -Year 2018 -Month 1 -Day 1 -hour 0 -Minute 0 -Second 0
+
+#Creates a fixed date time
+$lastDay_int = [DateTime]::DaysInMonth($today.Year, $today.Month)  # DaysInMonth is a static member
+$LastDayThisMonth = [DateTime]::new(2018,1,1)
+$LastDayThisMonth
+
+
+# -Format outputs other data types
+$thisyear = get-date -Format yyyy #thisyear is an integer
 $lastyear = $thisyear-1
+$lastyear # this is a integer
 
-$lastyear # this is a int
 
-#get-date | gm
+#get-date | gm # gm is alias for Get-Member
 
-#get-date -Format yyyy
 
 $NextYear = (get-date).AddYears(1)
 
-$NextYear # This a a date
+$NextYear # This a a datetime
 
-$NextYear.Year
+$NextYear.Year #this outputs an integer
+
+#get-date kan have a variable as parameter. 
+#get-date $adate -format yyyy
 
 
-$adate = get-date -Year 2018 -Month 1 -Day 1
 
-get-date $adate -format yyyy
 
-#$adate |gm
-
-        $today = get-date
-        $lastDay = [DateTime]::DaysInMonth($today.Year, $today.Month)
-        $lastDay
-    $today.AddMonths(-1)
 
