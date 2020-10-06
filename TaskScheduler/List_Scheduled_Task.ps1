7d79052e1ed663794a95db5a93162cd8b542b00e
bjorn76 <bjorn.k.j@gmail.com>
2020-10-06 19:17:26 +0200

    B2: Under dev

--- TaskScheduler/List_Scheduled_Task.ps1
+++ TaskScheduler/List_Scheduled_Task.ps1
@@ -1,8 +1,12 @@
 ﻿# List_Scheduled_Task.ps1
 # Must run as administrator
 # History:
-# 
-#  
+# 1) List all scheudled task
+# 2) Lis info on first and 2nd objects
+# 3) List info on all objects
+# 5) Join TaskNames
+
+
 
 
 
@@ -18,23 +22,27 @@ $FoundInfo2
 
 Write-Host -nonewline -foregroundcolor cyan "Found: "
 
+
+
+# 1) Start of 1
 $Found =  Get-ScheduledTask -TaskPath "*"
 
-#$Found =  Get-ScheduledTaskInfo -Taskname "Schedule Scan"
 
 
-# $Found | Format-Table | Export-Csv -Path c:\temp\ScheduledTask.csv
 
-# $Found | Format-List
-# $Found | Format-Table -AutoSize
-
-#$Found | Select-Object Taskname, Status |  Export-Csv -Path c:\temp\ScheduledTask.csv
-#$Found | Select-Object Taskname, Status  | Format-Table #| Out-File c:\temp\ScheduledTask.csv
-#$Found | Select-Object *  | Format-Table
 #$Found | Format-Table
+
+
+
+# 2)     Start of 2
+
 
 $FoundInfo1 = Get-ScheduledTaskInfo -InputObject $Found[1]
 $FoundInfo2 = Get-ScheduledTaskInfo -InputObject $Found[2]
+
+
+$FoundInfo3= $FoundInfo1 #+ $FoundInfo2
+
 
 $i = 0
 
@@ -58,10 +66,11 @@ $i +=1
 $FoundInfo1 |format-table
 $FoundInfo2 |format-List
 
-$FoundInfo = $FoundInfo1 AND $FoundInfo2
+
 
 
 #$Found | Sort-Object Taskname | Export-Csv -Path c:\temp\ScheduledTask.csv
 
 
 
+# Start of slask lines
