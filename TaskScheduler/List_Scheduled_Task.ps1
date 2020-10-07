8f6bec4ab362970dc2075f325d4443f73e0e4c51
bjorn76 <bjorn.k.j@gmail.com>
2020-10-07 07:23:39 +0200

    B2:in progress

--- TaskScheduler/List_Scheduled_Task.ps1
+++ TaskScheduler/List_Scheduled_Task.ps1
@@ -8,57 +8,57 @@
 
 
 
-
-
 cls
+Clear-Variable Info0, Info1, Info2, Info3, Info4
+Clear-Variable TaskInfo, TaskInfo_ 
 
-$Found
-$aSchTask
-$FoundInfo
-$FoundInfo1
-$FoundInfo2
-
-#cls
-
-
-
-# 1) Start of 1
+# 1) Start of 
 
 Write-Host -nonewline -foregroundcolor cyan "Get all Tasks Scheduled: "
-$Found =  Get-ScheduledTask -TaskPath "*"
-$Found | Format-Table
+$AllScheduledTasks =  Get-ScheduledTask -TaskPath "\Microsoft*"
+#$AllScheduledTasks | Format-Table
 
 
 
 # 2)     Start of 2
-Write-Host -nonewline -foregroundcolor cyan "More info on the first 2 Tasks Scheduled: "
+#Write-Host -nonewline -foregroundcolor cyan "More info on the first 2 Tasks Scheduled: "
 
-$FoundInfo1 = Get-ScheduledTaskInfo -InputObject $Found[1]
-$FoundInfo2 = Get-ScheduledTaskInfo -InputObject $Found[2]
+$Info0 = Get-ScheduledTaskInfo -InputObject $AllScheduledTasks[0]
+$Info1 = Get-ScheduledTaskInfo -InputObject $AllScheduledTasks[1]
+$Info2 = Get-ScheduledTaskInfo -InputObject $AllScheduledTasks[2]
+#$Info3 = Get-ScheduledTaskInfo -InputObject $AllScheduledTasks[3]
+#$Info4 = Get-ScheduledTaskInfo -InputObject $AllScheduledTasks[4]
 
 
-$FoundInfo1 | format-List
-$FoundInfo2 | format-List
+
+$Info12 = $Info0, $Info1, $Info2, $Info3, $Info4
+$Info12 | Format-List
+
+
+#$Info122 = $Info12 , $Info2
+#$Info122 | Format-Table
+
 
 # 3)     Start of 3
 Write-Host -nonewline -foregroundcolor cyan "More info on ALL Tasks Scheduled: "
 
-$i = 0
 
-foreach ($aSchTask in $Found) {
-  Get-ScheduledTaskInfo -InputObject $aSchTask      
-      
 
-  $i +=1
+
+#foreach ($Task in $AllScheduledTasks) {
+#For($i=0; $i -le $AllScheduledTasks.count ; $i++) {
+
+For($i=0; $i -le 2 ; $i++) {
+ 
+  $TaskInfo_ = Get-ScheduledTaskInfo -InputObject $AllScheduledTasks[$i] 
+  $TaskInfo =   $TaskInfo, $TaskInfo_     
+
 
 }
 
-$FoundInfo =  Get-ScheduledTaskInfo -InputObject  $Found[2]
-$FoundInfo | Format-Table
 
-
-$FoundInfo = $FoundInfo1, $FoundInfo2
-
+$TaskInfo | Format-list
+$i
 
 
 
