2570cd9b74c6b76c1ba21650c34bc608414f7af7
bjorn76 <bjorn.k.j@gmail.com>
2022-01-15 15:48:24 +0100

    init

---
+++ IE/function Set-ChromeAsDefaultBrowser.ps1
@@ -0,0 +1,6 @@
+﻿function Set-ChromeAsDefaultBrowser {
+    Add-Type -AssemblyName 'System.Windows.Forms'
+    Start-Process $env:windir\system32\control.exe -ArgumentList '/name Microsoft.DefaultPrograms /page pageDefaultProgram\pageAdvancedSettings?pszAppName=google%20chrome'
+    Sleep 2
+    [System.Windows.Forms.SendKeys]::SendWait("{TAB} {TAB}{TAB} ")
+}
